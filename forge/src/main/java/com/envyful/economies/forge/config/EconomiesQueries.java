bbea0c9b1f5b77ca229c8502c1f49c48b788a050
danorris709 <dnorris443@gmail.com>
2021-09-07 15:27:54 +0100

    feat(forge): add economy queries

---
+++ forge/src/main/java/com/envyful/economies/forge/config/EconomiesQueries.java
@@ -0,0 +1,18 @@
+package com.envyful.economies.forge.config;
+
+public class EconomiesQueries {
+
+    public static final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS `forge_economies_banks`(" +
+            "id         INT             UNSIGNED    NOT NULL    AUTO_INCREMENT, " +
+            "uuid       VARCHAR(64)     NOT NULL, " +
+            "economy    VARCHAR(200)    NOT NULL, " +
+            "balance    FLOAT(7, 4)     NOT NULL, " +
+            "UNIQUE(uuid, economy), " +
+            "PRIMARY KEY(id));";
+
+    public static final String LOAD_USER = "SELECT economy, balance FROM `forge_economies_banks` WHERE uuid = ?;";
+
+    public static final String CREATE_OR_UPDATE_ACCOUNT = "INSERT INTO `forge_economies_banks`(uuid, economy, balance) " +
+            "VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE balance = VALUES(`balance`);";
+
+}
