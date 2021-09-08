65fdbd3400a028c8eab1604a600e57882de1b9d5
danorris709 <dnorris443@gmail.com>
2021-09-08 18:47:55 +0100

    feat(forge): add economies to config

--- forge/src/main/java/com/envyful/economies/forge/config/EconomiesConfig.java
+++ forge/src/main/java/com/envyful/economies/forge/config/EconomiesConfig.java
@@ -3,7 +3,13 @@ package com.envyful.economies.forge.config;
 import com.envyful.api.config.data.ConfigPath;
 import com.envyful.api.config.type.SQLDatabaseDetails;
 import com.envyful.api.config.yaml.AbstractYamlConfig;
+import com.envyful.economies.api.Economy;
+import com.envyful.economies.forge.impl.ForgeEconomy;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+
+import java.util.Map;
 
 @ConfigPath("config/EconomiesForge/config.yml")
 @ConfigSerializable
@@ -12,11 +18,41 @@ public class EconomiesConfig extends AbstractYamlConfig {
     private SQLDatabaseDetails database = new SQLDatabaseDetails("EconomiesForge", "0.0.0.0", 3306,
             "admin", "password", "eco");
 
+    private Map<String, ConfigEconomy> economies = Maps.newHashMap(ImmutableMap.of(
+            "one", new ConfigEconomy("one", "dollars", "$", true)
+    ));
+
     public EconomiesConfig() {
         super();
     }
 
     public SQLDatabaseDetails getDatabase() {
         return this.database;
+    }
+
+    public Map<String, ConfigEconomy> getEconomies() {
+        return this.economies;
+    }
+
+    public static class ConfigEconomy {
+
+        private String id;
+        private String displayName;
+        private String identifier;
+        private boolean prefix;
+
+        public ConfigEconomy(String id, String displayName, String identifier, boolean prefix) {
+            this.id = id;
+            this.displayName = displayName;
+            this.identifier = identifier;
+            this.prefix = prefix;
+        }
+
+        public ConfigEconomy() {
+        }
+
+        public Economy getEconomy() {
+            return new ForgeEconomy(this.id, this.displayName, this.identifier, this.prefix);
+        }
     }
 }
