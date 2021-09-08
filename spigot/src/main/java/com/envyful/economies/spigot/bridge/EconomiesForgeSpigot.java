96123e9cf0c816fb5096d84c3cac24021dadab08
danorris709 <dnorris443@gmail.com>
2021-09-08 22:44:31 +0100

    feat(spigot): begin spigot bridge

---
+++ spigot/src/main/java/com/envyful/economies/spigot/bridge/EconomiesForgeSpigot.java
@@ -0,0 +1,11 @@
+package com.envyful.economies.spigot.bridge;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EconomiesForgeSpigot extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
