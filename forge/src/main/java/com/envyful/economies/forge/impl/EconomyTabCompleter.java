6e15295f22368b81b04412117c31375164dd6560
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-10-06 19:23:28 +0100

    fix(forge): change the tab completions to be proper ids

--- forge/src/main/java/com/envyful/economies/forge/impl/EconomyTabCompleter.java
+++ forge/src/main/java/com/envyful/economies/forge/impl/EconomyTabCompleter.java
@@ -3,6 +3,8 @@ package com.envyful.economies.forge.impl;
 import com.envyful.api.command.injector.TabCompleter;
 import com.envyful.economies.api.Economy;
 import com.envyful.economies.forge.EconomiesForge;
+import com.envyful.economies.forge.config.EconomiesConfig;
+import com.google.common.collect.Lists;
 import net.minecraft.entity.player.EntityPlayerMP;
 
 import java.lang.annotation.Annotation;
@@ -10,6 +12,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EconomyTabCompleter implements TabCompleter<Economy, EntityPlayerMP> {
+
+    private List<String> keysCache = null;
+
     @Override
     public Class<EntityPlayerMP> getSenderClass() {
         return EntityPlayerMP.class;
@@ -23,6 +28,19 @@ public class EconomyTabCompleter implements TabCompleter<Economy, EntityPlayerMP
     @Override
     public List<String> getCompletions(EntityPlayerMP sender, String[] currentData,
                                        Annotation... completionData) {
-        return new ArrayList<>(EconomiesForge.getInstance().getConfig().getEconomies().keySet());
+        return new ArrayList<>(this.getKeys());
+    }
+
+    private List<String> getKeys() {
+        if (this.keysCache == null) {
+            List<String> keys = Lists.newArrayList();
+
+            for (EconomiesConfig.ConfigEconomy value : EconomiesForge.getInstance().getConfig().getEconomies().values()) {
+                keys.add(value.getEconomy().getId());
+            }
+            this.keysCache = keys;
+        }
+
+        return this.keysCache;
     }
 }
