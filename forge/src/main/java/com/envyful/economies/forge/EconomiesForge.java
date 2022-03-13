1ef1ceb6fcfdf57924bd706787e95b51bae20fc9
Daniel Norris <dnorris443@gmail.com>
2022-03-13 14:17:00 +0000

    chore(gradle): bump version to 1.7.3

--- forge/src/main/java/com/envyful/economies/forge/EconomiesForge.java
+++ forge/src/main/java/com/envyful/economies/forge/EconomiesForge.java
@@ -39,7 +39,7 @@ import java.sql.SQLException;
 )
 public class EconomiesForge {
 
-    protected static final String VERSION = "1.7.2";
+    protected static final String VERSION = "1.7.3";
 
     private static EconomiesForge instance;
 
